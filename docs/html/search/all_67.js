4f2bb1b790cdbb8131065d279afbcdab60437b20
surajvijayan <suraj@broadcom.com>
2013-08-17 12:09:49 -0700

    Updated docs

--- docs/html/search/all_67.js
+++ docs/html/search/all_67.js
@@ -4,6 +4,5 @@ var searchData=
   ['generic_5fplugin_2eh',['generic_plugin.h',['../db/d97/generic__plugin_8h.html',1,'']]],
   ['generic_5fserver',['generic_server',['../d1/db4/classgeneric__server.html',1,'']]],
   ['generic_5fserver_2eh',['generic_server.h',['../dc/ded/generic__server_8h.html',1,'']]],
-  ['get_5fplugin_5fparams',['get_plugin_params',['../da/df5/classgeneric__plugin.html#acf52343d11812d28469e6a79de5449d6',1,'generic_plugin::get_plugin_params()'],['../d3/ded/classsample__plugin.html#a45525ac5128482386a90d1b75d2f5887',1,'sample_plugin::get_plugin_params()']]],
-  ['github',['GitHub',['../d1/df0/GPA.html',1,'']]]
+  ['get_5fplugin_5fparams',['get_plugin_params',['../da/df5/classgeneric__plugin.html#acf52343d11812d28469e6a79de5449d6',1,'generic_plugin::get_plugin_params()'],['../d3/ded/classsample__plugin.html#a45525ac5128482386a90d1b75d2f5887',1,'sample_plugin::get_plugin_params()']]]
 ];
